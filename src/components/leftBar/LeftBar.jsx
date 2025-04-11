1d2c0337a89c8deb4779753b59d816bd57cd897b
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-11 22:51:09 +0600

    added icons styles in leftBar component

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -2,8 +2,27 @@ import "./leftBar.css"
 
 const LeftBar = () => {
     return (
-        <div className="letBar">
-            left Bar
+        <div className="leftBar">
+            <div className="menuIcons">
+                <a href="" className="menuIcon">
+                    <img src="/general/logo.png" alt=""  className="logo"/>
+                </a>
+                <a href="" className="menuIcon">
+                    <img src="/general/home.svg" alt="" />
+                </a>
+                <a href="" className="menuIcon">
+                    <img src="/general/create.svg" alt="" />
+                </a>
+                <a href="" className="menuIcon">
+                    <img src="/general/updates.svg" alt="" />
+                </a>
+                <a href="" className="menuIcon">
+                    <img src="/general/messages.svg" alt="" />
+                </a>
+            </div>
+            <a href="" className="menuIcon">
+                    <img src="/general/messages.svg" alt="" />
+                </a>
         </div>
     );
 };
