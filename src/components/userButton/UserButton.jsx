a3e9f0d2831818d189c44ee7285e7510fcb6c616
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 00:06:43 +0600

    Style the user Button

--- src/components/userButton/UserButton.jsx
+++ src/components/userButton/UserButton.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import "./userButon.css"
 
 const UserButton = () => {
-    return (
+    const [open, setOpen] = useState()
+    // TEMP
+    const currentUser = true
+    return  currentUser ? (
         <div className="userButton">
-            User
+            <img src="/general/noAvatar.png" alt="" />
+            <img onClick={() => setOpen((prev) => !prev)} src="/general/arrow.svg" alt="" className="arrow"/>
+            {open && <div className="userOptions">
+                <div className="userOption">Profile</div>
+                <div className="userOption">Setting</div>
+                <div className="userOption">Logout</div>
+            </div>}
         </div>
-    );
+    ) : (
+        <a href="/" className="loginLink">
+            login / sign up
+        </a>
+    )
+       
+    
 };
 
 export default UserButton;
