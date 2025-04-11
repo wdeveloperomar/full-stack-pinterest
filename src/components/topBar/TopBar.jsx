38310a8552ee76c14c561199c1d0de5ca71baa86
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-11 22:55:06 +0600

    added search icon and input in topbar component

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -3,7 +3,12 @@ import "./topBar.css"
 const TopBar = () => {
     return (
         <div className="topBar">
-            top bar
+           {/* SERCH */}
+           <div className="search">
+                <img src="/general/search.svg" alt="" />
+                <input type="text" name="" id="" placeholder="Search" />
+           </div>
+           {/* USER */}
         </div>
     );
 };
