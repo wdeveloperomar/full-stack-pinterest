920d80ef5c54d27f8e5111b8cf3621b54e0aed49
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 02:20:27 +0600

    added image overlay styles and buttons

--- src/components/galleryItem/GalleryItem.jsx
+++ src/components/galleryItem/GalleryItem.jsx
@@ -1,9 +1,20 @@
 import "./galleryItem.css"
+import {Link} from "react-router"
 
 const GalleryItem = ({item}) => {
     return (
         <div className="galleryItem" style={{gridRowEnd:`span ${Math.ceil(item.height/100)}`}}>
            <img src={item.media} alt="" />
+           <Link to={`/pin/${item.id}`} className="overlay"></Link>
+           <button className="saveButton">Save</button>
+           <div className="overlayIcons">
+            <button>
+                <img src="/general/share.svg" alt="" />
+            </button>
+            <button>
+                <img src="/general/more.svg" alt="" />
+            </button>
+           </div>
         </div>
     );
 };
