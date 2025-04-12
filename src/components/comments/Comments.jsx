2e325763c76450f59c17b42e0c2bbdd1dfcb1e46
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 12:54:50 +0600

    added comment details and emoji picker and styles

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,9 +1,55 @@
 import "./comments.css"
+import Image from "../image/Image"
+import EmojiPicker from 'emoji-picker-react';
+import { useState } from "react";
+
+
 
 const Comments = () => {
+    const [open, setOpen] = useState(false)
     return (
         <div className="comments">
-            comments
+            <div className="commentList">
+                <span className="commentCount">5 comments</span>
+                {/* Comment */}
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt=""></Image>
+                    <div className="commentContent">
+                        <span className="commentUserName">Omar Faruk</span>
+                        <p className="commentText">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <span className="commentTime">
+                            1h
+                        </span>
+                    </div>
+                </div>
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt=""></Image>
+                    <div className="commentContent">
+                        <span className="commentUserName">Omar Faruk</span>
+                        <p className="commentText">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <span className="commentTime">
+                            1h
+                        </span>
+                    </div>
+                </div>
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt=""></Image>
+                    <div className="commentContent">
+                        <span className="commentUserName">Omar Faruk</span>
+                        <p className="commentText">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <span className="commentTime">
+                            1h
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <form className="commentForm">
+                <input type="text" name="" id="" placeholder="Add a comment"/>
+                <div onClick={() => setOpen((prev) => !prev)} className="emoji">😊</div>
+                {open && <div className="emojiPicker">
+                    <EmojiPicker></EmojiPicker>
+                </div>}
+            </form>
         </div>
     );
 };
