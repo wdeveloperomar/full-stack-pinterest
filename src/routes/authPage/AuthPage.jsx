ad91a015987cf26236ed9579d46a5a7be3c627c0
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 16:46:14 +0600

    create login and register page

--- src/routes/authPage/AuthPage.jsx
+++ src/routes/authPage/AuthPage.jsx
@@ -1,9 +1,62 @@
 import "./authPage.css";
+import Image from "../../components/image/Image"
+import { useState } from "react";
 
 const AuthPage = () => {
+
+    const [isRegister, setIsRegister] = useState(false)
+    const [error, setError] = useState("")
     return (
         <div className='authPage'>
-            Auth Page
+           <div className="authContainer">
+            <Image path="/general/logo.png" alt="" w={36} h={36}></Image>
+            <h1>
+                {isRegister ? "Create an Account" : "Login to your account"}
+            </h1>
+            {
+                isRegister ? (
+                    <form key="register">
+                        <div className="formGroup">
+                            <label htmlFor="username">User Name</label>
+                            <input type="username" name="username" id="username" placeholder="Username" required />
+                        </div>
+                        <div className="formGroup">
+                            <label htmlFor="displayName">Name</label>
+                            <input type="displayName" name="displayName" id="displayName" placeholder="Name" required />
+                        </div>
+                        <div className="formGroup">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" placeholder="Email" required />
+                        </div>
+                        <div className="formGroup">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" id="password" placeholder="Password" required />
+                        </div>
+                        <button type="submit">Register</button>
+                        <p onClick={() => setIsRegister(false)}>Do you have an account? <b>Login</b></p>
+                        {
+                            error && <p className="error">{error}</p>
+                        }
+                    </form>
+                ) : (
+                    <form key="login">
+                        <div className="formGroup">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" placeholder="Email" required />
+                        </div>
+                        <div className="formGroup">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" id="password" placeholder="Password" required />
+                        </div>
+                        <button type="submit">Login</button>
+                        <p onClick={() => setIsRegister(true)}>Don&apos;t  have an account? <b>Register</b></p>
+                        {
+                            error && <p className="error">{error}</p>
+                        }
+                    </form>
+                )
+            }
+           </div>
         </div>
     );
 };
