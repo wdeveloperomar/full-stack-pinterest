06df6cdf694cbc67125fabd240fcbf8f298765d8
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 16:11:37 +0600

    added details in create page and styles

--- src/routes/createpage/CreatePage.jsx
+++ src/routes/createpage/CreatePage.jsx
@@ -1,9 +1,52 @@
 import "./createPage.css"
+import Image from "../../components/image/Image"
 
 const CreatePage = () => {
     return (
         <div className='createPage'>
-           Creat Page 
+          <div className="createTop">
+            <h1>Create Pin</h1>
+            <button>Publish</button>
+          </div>
+          <div className="createBottom">
+            <div className="upload">
+                <div className="uploadTitle">
+                    <Image path="/general/upload.svg" alt=""></Image>
+                    <span>Choose a file</span>
+                </div>
+                <div className="uploadInfo">
+                    We recommend using high quality .jpg files less than 20 files less than 200 MB.
+                </div>
+            </div>
+            <form className="createForm">
+                <div className="createFormItem">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" id="title" placeholder="Add a title" />
+                </div>
+                <div className="createFormItem">
+                    <label htmlFor="description">Description</label>
+                    <textarea rows={6} type="text" name="description" id="description" placeholder="Add a detail description" />
+                </div>
+                <div className="createFormItem">
+                    <label htmlFor="link">Link</label>
+                    <input type="text" name="link" id="link" placeholder="Add a link" />
+                </div>
+                <div className="createFormItem">
+                    <label htmlFor="board">Board</label>
+                    <select name="board" id="board">
+                        <option value="">Choose a board</option>
+                        <option value="1">Board 1</option>
+                        <option value="2">Board 2</option>
+                        <option value="3">Board 3</option>
+                    </select>
+                </div>
+                <div className="createFormItem">
+                    <label htmlFor="tags">Tagged topics</label>
+                    <input type="text" name="tags" id="tags" placeholder="Add tags" />
+                    <small>Don't worry, people won't see your tags.</small>
+                </div>
+            </form>
+          </div>
         </div>
     );
 };
