3581a45962c7e82f4977b79108512821d39fbf86
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 12:07:15 +0600

    added details and styles in postInteraction

--- src/components/postInteraction/PostInteraction.jsx
+++ src/components/postInteraction/PostInteraction.jsx
@@ -1,9 +1,16 @@
+import Image from "../image/Image";
 import "./postInteraction.css"
 
 const PostInteraction = () => {
     return (
         <div className="postInteraction">
-            post inter action
+           <div className="interactionIcons">
+                <Image path="/general/react.svg"></Image>
+                273
+                <Image path="/general/share.svg"></Image>
+                <Image path="/general/more.svg"></Image>
+           </div>
+           <button>Save</button>
         </div>
     );
 };
