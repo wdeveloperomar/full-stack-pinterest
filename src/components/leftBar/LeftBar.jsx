34147f9890ed6868dfb57bc93de558dd99f6eb96
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 15:19:39 +0600

    chage a to Link

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,29 +1,30 @@
 import Image from "../image/Image";
 import "./leftBar.css"
+import {Link} from "react-router"
 
 const LeftBar = () => {
     return (
         <div className="leftBar">
             <div className="menuIcons">
-                <a href="" className="menuIcon">
+                <Link to="/" className="menuIcon">
                     <Image path="/general/logo.png" alt=""  className="logo"/>
-                </a>
-                <a href="" className="menuIcon">
+                </Link>
+                <Link to="/" className="menuIcon">
                     <Image path="/general/home.svg" alt="" />
-                </a>
-                <a href="" className="menuIcon">
+                </Link>
+                <Link to="/create" className="menuIcon">
                     <Image path="/general/create.svg" alt="" />
-                </a>
-                <a href="" className="menuIcon">
+                </Link>
+                <Link to="" className="menuIcon">
                     <Image path="/general/updates.svg" alt="" />
-                </a>
-                <a href="" className="menuIcon">
+                </Link>
+                <Link to="" className="menuIcon">
                     <Image path="/general/messages.svg" alt="" />
-                </a>
+                </Link>
             </div>
-            <a href="" className="menuIcon">
+            <Link to="" className="menuIcon">
                     <Image path="/general/settings.svg" alt="" />
-                </a>
+                </Link>
         </div>
     );
 };
