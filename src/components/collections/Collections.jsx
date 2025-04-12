036f3a72c6ab0f41e7c72c342fb063b44f8f6f42
Omar Faruk <154871609+wdeveloperomar@users.noreply.github.com>
2025-04-12 15:15:46 +0600

    create collections

--- src/components/collections/Collections.jsx
+++ src/components/collections/Collections.jsx
@@ -1,9 +1,121 @@
 import "./collections.css";
+import Image from "../image/Image"
 
 const Collections = () => {
     return (
         <div className="collections">
-            collections
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
+            {/* collections */}
+            <div className="collection">
+                <Image path="/pins/pin1.jpeg" alt=""></Image>
+                <div className="collectionInfo">
+                    <h1>Minimalist Bedrooms</h1>
+                    <span>12 pins . 1 w</span>
+                </div>
+            </div>
         </div>
     );
 };
